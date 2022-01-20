15d251fe206e71ff5859858176e365dc384ad7b3
Your Name <you@example.com>
2022-01-20 18:32:47 +0100

    added cleaned code

--- quiz.js
+++ quiz.js
@@ -50,34 +50,49 @@ function init() {
 let rightAnswer = 0; //variable für anzeige der richtig beantworteten Fragen auf dem endscreen.
 
 function showQuestion() {
-
-    if //show end screen
-    (currentQuestion >= questions.length) { //wenn currentquestion länger/größer oder gleich als/wie Array(5) ist
-        document.getElementById('endBody').style = ''; //display-none wird von style in html entfernt, endscreen wird nun angezeigt
-        document.getElementById('startBody').style = 'display: none'; //active screen bekommt display none
-
-        document.getElementById('sumOfRightAnswers').innerHTML = rightAnswer; //variable für Anzahl der richtig beantworteten Fragen auf dem endscreen
-    
-    } else { //show question
- 
-        let percent = currentQuestion / questions.length; //aktuelle Frage soll durch die Gesamtlänge (akso 5) des Arrays geteilt werden, um Prozentangabe anzuzeigen
-        percent = percent * 100; //Ergebnis wird in vollen Zahlen und % angegeben durch multiplizieren mit 100
-        document.getElementById('progressBar').innerHTML = `${percent}%`; //Prozent-Wert wird eingegeben durch Variable percent und Berechnung
-        document.getElementById('progressBar').style.width = `${percent}%`; //width-Wert aus html wird geändert mit style
-
-        console.log('Fortschritt', percent);
-
-        let question = questions[currentQuestion]; //Variable, die aus Array "questions" das erste JSON durch die globale Variable "currentQuestion" an der Stelle 0/also erstes Array rauslesen soll
-
-        document.getElementById('questionNumber').innerHTML = currentQuestion + 1; //die Nr der Frage/Karte wird unten angezeigt, ist variabel, beginnt mit 1 zu zählen an durch +1, weil Js immer mit 0 beginnt zu zählen
-        document.getElementById('currQuestion').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-
-        document.getElementById('switchImg').src = './img/cardimg.png';
+    if  (gameIsOver()) { //wenn currentquestion länger/größer oder gleich als/wie Array(5) ist
+        showEndScreen();
+    } else {              //show question
+        updateProgressBar();
+        updateNextQuestion();
     };
 }
+
+function gameIsOver(){
+return currentQuestion >= questions.length;
+}
+
+function showEndScreen() {
+
+    document.getElementById('endBody').style = ''; //display-none wird von style in html entfernt, endscreen wird nun angezeigt
+    document.getElementById('startBody').style = 'display: none'; //active screen bekommt display none
+
+    document.getElementById('sumOfRightAnswers').innerHTML = rightAnswer; //variable für Anzahl der richtig beantworteten Fragen auf dem endscreen
+}
+
+function updateProgressBar() {
+
+    let percent = currentQuestion / questions.length; //aktuelle Frage soll durch die Gesamtlänge (akso 5) des Arrays geteilt werden, um Prozentangabe anzuzeigen
+    percent = percent * 100; //Ergebnis wird in vollen Zahlen und % angegeben durch multiplizieren mit 100
+    document.getElementById('progressBar').innerHTML = `${percent}%`; //Prozent-Wert wird eingegeben durch Variable percent und Berechnung
+    document.getElementById('progressBar').style.width = `${percent}%`; //width-Wert aus html wird geändert mit style
+
+    console.log('Fortschritt', percent);
+}
+
+function updateNextQuestion() {
+
+    let question = questions[currentQuestion]; //Variable, die aus Array "questions" das erste JSON durch die globale Variable "currentQuestion" an der Stelle 0/also erstes Array rauslesen soll
+
+    document.getElementById('questionNumber').innerHTML = currentQuestion + 1; //die Nr der Frage/Karte wird unten angezeigt, ist variabel, beginnt mit 1 zu zählen an durch +1, weil Js immer mit 0 beginnt zu zählen
+    document.getElementById('currQuestion').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+
+    document.getElementById('switchImg').src = './img/cardimg.png';
+}
+
 
 function answer(currAnswer) { //Variable wird in die Klammer gegeben,da es 3 Antwort-Varianten gibt
     let question = questions[currentQuestion];
@@ -87,8 +102,8 @@ function answer(currAnswer) { //Variable wird in die Klammer gegeben,da es 3 Ant
     console.log('current question is', question['right_answer']); //Nr hinter right_answer wird rausgelesen
 
     let idOfRightAnswer = `answer_${question['right_answer']}`; //Variable wird für Zeile 74 benötigt, um richtige Antwort grün zu färben, falls die falsche gewählt wird
-    //hinter $-Zeichen: answer_(die jeweilige Nummer(Zeile 64), die die rihtige Antwort im Array anzeigt)
-    if (selectedQuestionNumber == question['right_answer']) { //wenn die selectedNr der Nr in right_answer übereinstimmt, ist die Antwort richtig
+                                                            //hinter $-Zeichen: answer_(die jeweilige Nummer(Zeile 64), die die rihtige Antwort im Array anzeigt)
+    if (rightAnswerSelected(selectedQuestionNumber, question)) { //wenn die selectedNr der Nr in right_answer übereinstimmt, ist die Antwort richtig
         document.getElementById('switchImg').src = './img/cardimg.png';
 
         console.log('Richtige Antwort!');
@@ -102,13 +117,19 @@ function answer(currAnswer) { //Variable wird in die Klammer gegeben,da es 3 Ant
         console.log('Falsche Antwort!'); //wenn die Nummern nicht übereinstimmen, ist es die falsche Antwort
         document.getElementById(currAnswer).classList.add('bg-danger'); //Dann färbt sich button rot
         document.getElementById(idOfRightAnswer).classList.add('bg-success');  //wenn die Antwort falsch ist, färbe mir die richtige Antwort rot
-     
+
         audioFail.play();
     }
 
     document.getElementById('nextQuestion').disabled = false;  //button soll mit disabled statisch werden, solange keine Antwort geklickt wird
 }
-//shows next Array by click on button
+
+function rightAnswerSelected(selectedQuestionNumber, question){
+    return selectedQuestionNumber == question['right_answer'];
+}
+
+
+//shows next JSON by click on button
 function showNextQuestion() {
     currentQuestion++; //diese globale Variable braucht ++, um den Wert den sie ursprünglich hatte (0) immer um 1 zu erhöhen
     document.getElementById('nextQuestion').disabled = true; //button function wird ausgesetzt
@@ -125,12 +146,12 @@ function editButtons() {
     document.getElementById('answer_3').classList.remove('bg-success');
 }
 
-function restartGame(){
+function restartGame() {
     document.getElementById('switchImg').src = './img/cardimg.png';
     document.getElementById('endBody').style = 'display: none';
     document.getElementById('startBody').style = '';
 
-    rightAnswer = 0; 
+    rightAnswer = 0;
     currentQuestion = 0;
 
     init();
